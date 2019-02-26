0dae77de88c04c29ba82f66db3cc4ff537d7fe33
birth1984 <88521762@qq.com>
2019-02-26 18:21:38 +0800

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private Rect mRectValue;
+    private Texture texture;
+
+    public Rect MRectValue { get => mRectValue; set => mRectValue = value; }
+    public Texture Texture { get => texture; set => texture = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+
+}
